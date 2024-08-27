966a24df8c10a631f6eccc5526a5d6f72a8bed2a
Pavel Boyko <p.boyko@tinkoff.ru>
2024-08-27 10:49:15 +0300

    add git pull to batv1

--- START.bat
+++ START.bat
@@ -1,7 +1,8 @@
 @echo off
 echo Activating virtual environment...
 call venv\Scripts\activate
-echo Starting the bot...
+echo Starting git pull
 git pull
+echo Starting the bot...
 python main.py -a 2
 pause
